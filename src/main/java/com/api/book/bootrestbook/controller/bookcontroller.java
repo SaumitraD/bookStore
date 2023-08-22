8ace7b557072fd32004bd8941fb4994a096b65bf
SaumitraD <hisaumitra@gmail.com>
2023-08-22 12:04:19 +0530

    database connectivity added

--- src/main/java/com/api/book/bootrestbook/controller/bookcontroller.java
+++ src/main/java/com/api/book/bootrestbook/controller/bookcontroller.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,7 +36,7 @@ public class bookcontroller {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
 
-        return ResponseEntity.of(Optional.of(list));
+        return ResponseEntity.status(HttpStatus.CREATED).body(list);
     }
 
     // get one book handler
--- src/main/java/com/api/book/bootrestbook/services/bookService.java
+++ src/main/java/com/api/book/bootrestbook/services/bookService.java
@@ -1,26 +1,30 @@
 package com.api.book.bootrestbook.services;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.api.book.bootrestbook.dao.BookRepository;
 import com.api.book.bootrestbook.entity.Book;
 
 @Service
 public class bookService {
-    private static List<Book> list = new ArrayList<>();
-    static {
-        list.add(new Book(12, "Java", "XYZ"));
-        list.add(new Book(123, "Python", "LMN"));
-        list.add(new Book(124, "C++", "ABC"));
-    }
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    // private static List<Book> list = new ArrayList<>();
+    // static {
+    // list.add(new Book(12, "Java", "XYZ"));
+    // list.add(new Book(123, "Python", "LMN"));
+    // list.add(new Book(124, "C++", "ABC"));
+    // }
 
     // get all books
 
     public List<Book> getAllBooks() {
+        List<Book> list = (List<Book>) this.bookRepository.findAll();
         return list;
     }
 
@@ -30,7 +34,9 @@ public class bookService {
         Book book = null;
 
         try {
-            book = list.stream().filter(e -> e.getId() == id).findFirst().get();
+            // book = list.stream().filter(e -> e.getId() == id).findFirst().get();
+            book = this.bookRepository.findByID(id);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -40,33 +46,38 @@ public class bookService {
     // add book
 
     public Book addBook(Book b) {
-        list.add(b);
-        return b;
+        // list.add(b);
+        Book result = this.bookRepository.save(b);
+        return result;
     }
 
     // delete book
 
     public void deleteBook(int bookid) {
 
-        list = list.stream().filter(book -> {
-            if (book.getId() != bookid) {
-                return true;
-            } else {
-                return false;
-            }
-        }).collect(Collectors.toList());
+        // list = list.stream().filter(book -> {
+        // if (book.getId() != bookid) {
+        // return true;
+        // } else {
+        // return false;
+        // }
+        // }).collect(Collectors.toList());
+
+        bookRepository.deleteById(bookid);
     }
 
     // update book
 
     public void updateBook(Book book, int bookid) {
 
-        list.stream().map(b -> {
-            if (b.getId() == bookid) {
-                b.setTitle(book.getTitle());
-                b.setAuthor(book.getAuthor());
-            }
-            return b;
-        }).collect(Collectors.toList());
+        // list.stream().map(b -> {
+        // if (b.getId() == bookid) {
+        // b.setTitle(book.getTitle());
+        // b.setAuthor(book.getAuthor());
+        // }
+        // return b;
+        // }).collect(Collectors.toList());
+        book.setId(bookid);
+        bookRepository.save(book);
     }
 }
